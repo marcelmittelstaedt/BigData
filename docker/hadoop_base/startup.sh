4c651d134758db534a9762f0d0393731eb64cf86
Marcel Mittelstaedt <mittelstaedtmarcel@googlemail.com>
2019-10-06 15:02:02 +0000

    Adjusted startup script to work even on restarts of container.

--- docker/hadoop_base/startup.sh
+++ docker/hadoop_base/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+# https://docs.docker.com/config/containers/multi-service_container/
 
 # Start SSH
-service ssh stop
 service ssh restart
 status=$?
 if [ $status -ne 0 ]; then
@@ -10,15 +10,28 @@ if [ $status -ne 0 ]; then
 fi
 
 # Fromat HDFS
-sudo -u hadoop -H sh -c "echo Switched to User:; whoami; cd; /home/hadoop/hadoop/bin/hdfs namenode -format"
-
-# Start Hadoop DFS and YARN
-sudo -u hadoop -H sh -c "echo Switched to User:; whoami; cd; /home/hadoop/hadoop/sbin/start-all.sh"
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start Hadoop DFS and Yarn: $status"
-  exit $status
+CONTAINER_ALREADY_INITIALIZED="CONTAINER_ALREADY_INITIALIZED"
+if [ ! -e $CONTAINER_ALREADY_INITIALIZED ]; then
+    touch $CONTAINER_ALREADY_INITIALIZED
+    echo "First start of container, will format HDFS."
+   
+	sudo -u hadoop -H sh -c "echo Switched to User:; whoami; cd; /home/hadoop/hadoop/bin/hdfs namenode -format"
+	status=$?
+	if [ $status -ne 0 ]; then
+  	  echo "Failed to format HDFS: $status"
+  	  exit $status
+	fi
+else
+    echo "Not first start of Container, no HDFS format necessary."
 fi
+
+# Start Hadoop Cluster
+#sudo -u hadoop -H sh -c "echo Switched to User:; whoami; cd; /home/hadoop/hadoop/sbin/start-all.sh"
+#status=$?
+#if [ $status -ne 0 ]; then
+#  echo "Failed to start Hadoop DFS and Yarn: $status"
+#  exit $status
+#fi
 
 while sleep 60; do
 	#ps aux | grep "hadoop" | grep -q -v grep
