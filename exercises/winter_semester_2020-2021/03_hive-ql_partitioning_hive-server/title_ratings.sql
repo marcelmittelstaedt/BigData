06b3c5cf1f05f581b9bb288a2a982b3e8c84d3d5
Marcel Mittelstaedt <marcel.mittelstaedt@datawhizz.io>
2021-02-21 18:38:50 +0100

    Added Slides for next lecture, solution for exercises of last lecture and excercises for next lecture.

---
+++ exercises/winter_semester_2020-2021/03_hive-ql_partitioning_hive-server/title_ratings.sql
@@ -0,0 +1,6 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS title_ratings(
+	tconst STRING, 
+	average_rating DECIMAL(2,1), 
+	num_votes BIGINT
+) COMMENT 'IMDb Ratings'ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' STORED AS TEXTFILE LOCATION '/user/hadoop/imdb/title_ratings' 
+TBLPROPERTIES ('skip.header.line.count'='1');
