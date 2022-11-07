4aebd9b47c2b3a64f81d093bc33c4634198314a5
Marcel Mittelstaedt <marcel.mittelstaedt@datawhizz.io>
2022-11-07 12:16:12 +0100

    Added slides

---
+++ solutions/winter_semester_2022-2023/03_hive-ql_partitioning_hive-server/name_basics_partitioned.sql
@@ -0,0 +1,9 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS name_basics_partitioned (
+		nconst STRING, 
+		primary_name STRING, 
+		birth_year INT, 
+		death_year STRING, 
+		primary_profession STRING, 
+		known_for_titles STRING
+) PARTITIONED BY (partition_is_alive STRING) 
+STORED AS PARQUET LOCATION '/user/hadoop/imdb/actors_partitioned';
